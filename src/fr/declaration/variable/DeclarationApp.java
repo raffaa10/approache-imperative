b2f4ec08ed0d8c1e4d191ab4b43d25ee2433c345
Rafayel AZIZYAN <rafayel.azizyan@gmail.com>
2020-06-24 02:31:28 +0200

    une classe manquante

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,28 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		
+		byte numeroDepartement = 34;
+		short numeroRue = 15;
+		int codePostal = 34_000;
+		long numeroHabitants = 25000L;
+		float moyenneParRue = 1.5f;
+		String nomRue = "Bonnes Nouvelles";
+		boolean presence = true; 
+		
+		// imprimer les variables
+		
+		System.out.println(numeroDepartement);
+		System.out.println(numeroRue);
+		System.out.println(codePostal);
+		System.out.println(numeroHabitants);
+		System.out.println(moyenneParRue);
+		System.out.println(nomRue);
+		System.out.println(presence);
+		
+
+	}
+
+}
